39a58ce02566031a555518271683352928c32494
Joonwoo <93jpark@gmail.com>
2023-10-17 20:08:21 +0900

    add docker-compose

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 cp ./init/env.startup.yaml ./environment/config/env.startup.yaml
 
-docker run \
-	--name ldap \
-	-v ./environment/config:/container/environment/01-custom \
-	-v ./init:/container/service/slapd/assets/config/bootstrap/ldif/custom \
-	-p 389:389 \
-	-p 636:636 \
-	osixia/openldap:1.5.0 --loglevel debug --copy-service
+docker-compose up
+
+# docker run \
+# 	--name ldap \
+# 	-v ./environment/config:/container/environment/01-custom \
+# 	-v ./init:/container/service/slapd/assets/config/bootstrap/ldif/custom \
+# 	-p 389:389 \
+# 	-p 636:636 \
+# 	osixia/openldap:1.5.0 --loglevel debug --copy-service
+
--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.ContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Autowired
+    @Bean
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .ldapAuthentication()
@@ -36,5 +37,6 @@ public class SecurityConfig {
                 .passwordEncoder(new BCryptPasswordEncoder())
                 .passwordAttribute("userPassword");
     }
+
 
 }
